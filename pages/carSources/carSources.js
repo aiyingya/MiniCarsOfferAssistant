823d094ee6d8d427d432d4061d6acaa42946350b
Panda Le <pandali@yaomaiche.com>
2017-02-09 19:42:29 +0800

    add file

--- pages/carSources/carSources.js
+++ pages/carSources/carSources.js
@@ -3,15 +3,33 @@ let config = require('../../lib/config.js');
 Page({
 	data: {
 		imgAliyuncsUrl: config.imgAliyuncsUrl,
-		carModelsInfo: ''
+		carModelsInfo: '',
+		windowHeight: '',
+		showRmendCarFacade:''
 	},
 	onLoad (options) {
 		let that = this;
 		let carModelsInfo = JSON.parse(options.carModelsInfo);
+		try {
+      var res = wx.getSystemInfoSync();
+			console.log(res)
+      this.pixelRatio = res.pixelRatio;
+      this.apHeight = 16;
+      this.offsetTop = 80;
+      this.setData({windowHeight: res.windowHeight + 'px'})
+    } catch (e) {
+      
+    }
 		console.log(carModelsInfo)
 		that.setData({
 			carModelsInfo: carModelsInfo
 		})
+	},
+	handlerAmendCarFacade(e) {
+		console.log(1);
+		let that = this;
+		that.setData({
+			showRmendCarFacade: true
+		})
 	}
-	
 })
