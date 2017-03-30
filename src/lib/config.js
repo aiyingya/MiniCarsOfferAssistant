55a9735de9d5d72940934fe9305fc5651f6b0130
David Fu <david.fu.zju.dev@gmail.com>
2017-03-30 14:33:41 +0800

    切换到生产环境

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,16 +1,5 @@
 /* 配置后台服务器地址 */
-const ENV = 'gqc'
 
-let ymcServerHTTPSUrl = (function () {
-  let HTTPS = {
-    dev: 'https://test.yaomaiche.com/ymcdev/',
-    gqc: 'https://test.yaomaiche.com/ymcgqc/',
-    prd: 'https://ymcapi.yaomaiche.com/ymc/'
-  }
-  return HTTPS[ENV]
-}())
+const ENV = 'prd'
+export default { ENV }
 
-module.exports = {
-  ymcServerHTTPSUrl: ymcServerHTTPSUrl,
-  ENV: ENV
-}
