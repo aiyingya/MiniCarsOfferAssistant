3876e1ca1a0175bf48d70cbc94f5ece1a57b4c20
David Fu <david.fu.zju.dev@gmail.com>
2017-04-20 19:21:56 +0800

    切换到 gqc

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 /**
  * 基础配置文件
  */
-const ENV = 'prd'
+const ENV = 'gqc'
 const name = 'yaomaiche-miniprogram'
-const version = '1.4.4'
+const version = '1.4.5'
 const build = 1
-const versionCode = '010404'
+const versionCode = '010405'
 
 const getNamespaceKey = function (key) {
   if (ENV === 'PRD') {
