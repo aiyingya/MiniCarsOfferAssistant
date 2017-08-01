39b76226e3918a238376c9fe699b1c9f00310882
David Fu <david.fu.zju.dev@gmail.com>
2017-08-01 16:30:21 +0800

    更新访客接口的返回体

--- flow-typed/myLibDef.js
+++ flow-typed/myLibDef.js
@@ -1,20 +1,44 @@
 /**
- * 普通访客用户实体, 此时的 status 为 normal
+ * 普通访客用户实体
  */
-declare type NormalGuestEntity = {
-  guestId: number,
+declare type GuestEntity = {
+  /**
+   * 访客标志
+   *
+   * @type {number}
+   */
+  guestId?: number,
+
+  /**
+   * 试用期创建/开始时间
+   * YYYY-mm-dd hh:MM:ss
+   *
+   * @type {string}
+   */
+  createTime?: string,
+
+  /**
+   * 试用期过期时间
+   * YYYY-mm-dd hh:MM:ss
+   *
+   * @type {string}
+   */
+  expireTime?: string,
+
+  /**
+   * 用户标示
+   *
+   * @type {string}
+   */
   userId: string,
-  createTime: string, // YYYY-mm-dd hh:MM:ss
-  expireTime: string, // YYYY-mm-dd hh:MM:ss
-  status: 'normal'
-};
 
-/**
- * 长期授权用户实体, 此时的  status 为 none
- */
-declare type NoneGuestEntity = {
-  status: 'none',
-  userId: string
+  /**
+   * 访客有三种状态
+   * normal 代表属于无登录权限的访客, 会拥有试用期
+   * none 代表费访客, 也就是正常用户
+   * expire 代表当前访客过期
+   *
+   * @type {('normal' | 'none' | 'expire')}
+   */
+  status: 'normal' | 'none' | 'expire'
 };
-
-declare type GuestEntity = NormalGuestEntity | NoneGuestEntity;
