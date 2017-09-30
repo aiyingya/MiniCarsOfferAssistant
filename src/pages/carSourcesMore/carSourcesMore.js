ecdb8f96b85cb7ab6c5197780aff649d715bf0e0
eliya <eliyaai@yaomaiche.com>
2017-09-30 17:27:07 +0800

    增加行情更多信息页面备注模板，该接口十一未确定，所以没有数据的页面不显示任何信息，开放部分静态页面

---
+++ src/pages/carSourcesMore/carSourcesMore.js
@@ -0,0 +1,46 @@
+// @flow
+import { container } from '../../landrover/business/index'
+const saasService: SAASService = container.saasService
+const userService: UserService = container.userService
+import util from '../../utils/util'
+
+Page({
+  data: {
+    pageIndex: 1,
+    pageSize: 10,
+    comments: [{
+      price: 0,
+      content: '', // 备注
+      userPhone: '', // 18888*****
+      updateTime: '', // 更新时间
+      tags: [] // 标签列表
+    }],
+    showCarModelName: '', // 要显示的车款标题
+    showColorName: '', // 要显示的内外饰颜色
+    itemId: 0, // 商品id
+    carSourceItem: {} // 车源信息吧
+  },
+  onLoad(options) {
+    this.setData({
+      carSourceItem : util.urlDecodeValueForKeyFromOptions('carSourceItem', options),
+      showCarModelName : options.showCarModelName,
+      showColorName : options.showColorName,
+      itemId : options.itemId
+    })
+    this.getData(options.itemId)
+  },
+  onShow() {
+  },
+  getData(itemId) {
+    saasService.getCarSourceMore(
+      itemId,
+      this.data.pageIndex,
+      this.data.pageSize
+    ).then(res => {
+      this.setData({
+        comments: res,
+      })
+      console.log("车源更多标签信息", this.data.comments)
+    })
+  }
+})
