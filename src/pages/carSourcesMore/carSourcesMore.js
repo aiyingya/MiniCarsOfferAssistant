e2c0bb538d772313f6e6fd1f5cd8ac63862784c5
eliya <eliyaai@yaomaiche.com>
2017-10-16 10:56:16 +0800

    我加了个分页

--- src/pages/carSourcesMore/carSourcesMore.js
+++ src/pages/carSourcesMore/carSourcesMore.js
@@ -15,6 +15,7 @@ Page({
       updateTime: '', // 更新时间
       tags: [] // 标签列表
     }],
+    infoItem: {},
     showCarModelName: '', // 要显示的车款标题
     showColorName: '', // 要显示的内外饰颜色
     carSourceId: 0, // 商品id
@@ -41,8 +42,25 @@ Page({
         res.content[index].updateTimeStr = util.getTimeStr(updateTime, 'YYYY/MM/DD hh:mm') // util.getTimeStr(updateTime, "yyyy-MM-dd")
       })
       this.setData({
+        infoItem: res,
         comments: res.content
       })
     })
-  }
+  },
+  handlerLoadMore() {
+    let newPageIndex = this.data.pageIndex + 1
+    saasService.getCarSourceMore(
+      this.data.carSourceId,
+      newPageIndex,
+      this.data.pageSize
+    ).then(res => {
+      res.content.length && res.content.forEach(({updateTime}, index) => {
+        res.content[index].updateTimeStr = util.getTimeStr(updateTime, 'YYYY/MM/DD hh:mm') // util.getTimeStr(updateTime, "yyyy-MM-dd")
+      })
+      this.setData({
+        infoItem: res,
+        comments: this.data.comments.concat(res.content)
+      })
+    })
+  },
 })
